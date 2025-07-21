319493bf154df430731828df34cd2d259d01be02
titopotito <jdf.tito@gmail.com>
2025-07-22 02:18:30 +0800

    Added keydown.enter functionality to input.

--- src/app/components/header/components/header-input.ts
+++ src/app/components/header/components/header-input.ts
@@ -4,7 +4,7 @@ import tdData from '../../../test-data/todo';
 @Component({
   selector: 'td-header-input',
   template: `<div>
-    <input type="text" #inputEl />
+    <input type="text" #inputEl (keydown.enter)="addTodo(inputEl)" />
     <button (click)="addTodo(inputEl)">+</button>
   </div>`,
   styleUrl: 'header-input.css',
@@ -12,6 +12,8 @@ import tdData from '../../../test-data/todo';
 export class HeaderInput {
   addTodo(inputEl: HTMLInputElement) {
     const todoText = inputEl.value;
+    if (todoText === '') return;
     tdData.push({ text: todoText, isDone: false });
+    inputEl.value = '';
   }
 }
